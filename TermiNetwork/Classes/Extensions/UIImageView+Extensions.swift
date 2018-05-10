190922ef770a3f78562278a5c68f97e98f8d60a5
Bill Panagiotopoulos <billp.dev@gmail.com>
2018-05-10 20:54:03 +0300

    Add test case for setRemoteImage & update docs

--- TermiNetwork/Classes/Extensions/UIImageView+Extensions.swift
+++ TermiNetwork/Classes/Extensions/UIImageView+Extensions.swift
@@ -10,6 +10,7 @@ import UIKit
 extension UIImageView {
     private static var activeCallsHashMap: [String: TNCall] = [:]
     
+ 
     fileprivate static func downloadImage(url: String, onSuccess: @escaping TNSuccessCallback<UIImage>, onFailure: @escaping TNFailureCallback) throws -> TNCall {
         let call = TNCall(method: .get, url: url, params: nil)
         call.skipBeforeAfterAllRequestsHooks = true
@@ -18,7 +19,17 @@ extension UIImageView {
         return call
     }
 
-    public func setRemoteImage(url: String, defaultImage: UIImage? = nil, preprocessImage: ((UIImage)->(UIImage))? = nil, beforeStart: (()->())? = nil, onFinish: ((UIImage?, Error?)->())? = nil) {
+    /**
+     Sets a remote image from url
+     
+     - parameters:
+         - url: The url of the remote image
+         - defaultImage: the UIImage showed while the image url is downloading (optional)
+         - beforeStart: a block of code executed before image download (optional)
+         - preprocessImage: a block of code that preprocess the image before showing. It should return the new image. This block will run in the background thread (optional)
+         - onFinish: a block of code executed after the completion of the download image request. It may fail so error will be returned in that case (optional)
+     */
+    public func setRemoteImage(url: String, defaultImage: UIImage? = nil, beforeStart: (()->())? = nil, preprocessImage: ((UIImage)->(UIImage))? = nil, onFinish: ((UIImage?, Error?)->())? = nil) {
         UIImageView.activeCallsHashMap[url]?.cancel()
         
         self.image = defaultImage
@@ -37,7 +48,6 @@ extension UIImageView {
                     onFinish?(image, nil)
                 }
             }
-            
         }, onFailure: { error, data in
             self.image = nil
             UIImageView.activeCallsHashMap[url] = nil
