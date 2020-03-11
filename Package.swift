d0b593e5eded7c07ec4771460155fc2f6421edb2
Vasilis Panagiotopoulos <v.panagiotopoulos@afse.eu>
2020-03-11 15:34:03 +0200

    Add package

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "TermiNetwork",
+    products: [
+        .library(
+            name: "TermiNetwork",
+            targets: ["TermiNetwork"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "TermiNetwork",
+            dependencies: []),
+    ]
+)
