a6f3bffa7146557fc9b722d6a0385dd41cb59190
Vasilis Panagiotopoulos <v.panagiotopoulos@afse.eu>
2020-03-12 13:34:50 +0200

    Update terms

--- TermiNetwork/Classes/TNRequest.swift
+++ TermiNetwork/Classes/TNRequest.swift
@@ -1,10 +1,21 @@
+// TNRequest.swift
 //
-//  TNRequest.swift
-//  TermiNetwork
+// Copyright © 2018-2020 Vasilis Panagiotopoulos. All rights reserved.
 //
-//  Created by Vasilis Panagiotopoulos on 14/02/2018.
-//  Copyright © 2018 Vasilis Panagiotopoulos. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in the
+// Software without restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in all copies
+// or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import Foundation
 import UIKit
@@ -32,9 +43,7 @@ public enum TNMethod: String {
     case patch
 }
 
-/**
- The the body type of the request
-*/
+/// The the body type of the request
 public enum TNRequestBodyType: String {
     /// The request params are sent as application/x-www-form-urlencoded mime type
     case xWWWFormURLEncoded = "application/x-www-form-urlencoded"
