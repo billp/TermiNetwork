e11b9e7b30f7b094f062e92629e5892b8b453e39
Bill Panagiotopoulos <billp.dev@gmail.com>
2020-12-20 01:22:44 +0200

    Add supported architectures to Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,14 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "TermiNetwork",
+    platforms: [
+      .macOS(.v10_15),
+      .iOS(.v13),
+      .tvOS(.v13),
+      .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "TermiNetwork",
